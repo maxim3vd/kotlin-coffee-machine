84219397a5471232b1477b17991c42164425756d
maxim3andr3 <maxim3.vd@gmail.com>
2024-07-21 18:25:20 +0200

    Add constants for WATER, MILK, and COFFEE_BEANS; Create skeleton for ingredientCalculator function

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,11 @@
+const val WATER = 200
+const val MILK = 50
+const val COFFEE_BEANS = 15
+
+fun ingredientCalculator(cupsNumber: Int) {
+
+}
+
 fun main() {
     println("Write how many cups of coffee you will need:")
     val cupsNumber: Int = readln().toInt()
