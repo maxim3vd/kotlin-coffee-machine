8e812e1966d5182b25e19cad6fd5b13c9612abf7
maxim3andr3 <maxim3.vd@gmail.com>
2024-07-21 18:30:59 +0200

    Add logic to ingredientCalculator function; Call the function in main with cupsNumber variable

--- src/Main.kt
+++ src/Main.kt
@@ -3,10 +3,16 @@ const val MILK = 50
 const val COFFEE_BEANS = 15
 
 fun ingredientCalculator(cupsNumber: Int) {
-
+    println("""
+        For $cupsNumber cups of coffee you will need:
+        ${WATER * cupsNumber} ml of water
+        ${MILK * cupsNumber} ml of milk
+        ${COFFEE_BEANS * cupsNumber} g of coffee beans
+    """.trimIndent())
 }
 
 fun main() {
     println("Write how many cups of coffee you will need:")
     val cupsNumber: Int = readln().toInt()
+    ingredientCalculator(cupsNumber)
 }
