2258e9866e8c7aea6d58ae65698316b04863463f
maxim3andr3 <maxim3.vd@gmail.com>
2024-07-21 18:17:54 +0200

    Prompt user for number of coffee cups and store in cupsNumber variable

--- src/Main.kt
+++ src/Main.kt
@@ -1,11 +1,4 @@
 fun main() {
-    println("""
-        Starting to make a coffee
-        Grinding coffee beans
-        Boiling water
-        Mixing boiled water with crushed coffee beans
-        Pouring coffee into the cup
-        Pouring some milk into the cup
-        Coffee is ready!
-    """.trimIndent())
+    println("Write how many cups of coffee you will need:")
+    val cupsNumber: Int = readln().toInt()
 }
