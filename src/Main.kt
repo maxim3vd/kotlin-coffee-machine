ad86872e17b9ffd390fa87e737e8a7d6874f92cc
maxim3andr3 <maxim3.vd@gmail.com>
2024-07-22 21:56:46 +0200

    Implement basic functionality to input and store ingredient quantities

--- src/Main.kt
+++ src/Main.kt
@@ -1,18 +1,8 @@
-const val WATER = 200
-const val MILK = 50
-const val COFFEE_BEANS = 15
-
-fun ingredientCalculator(cupsNumber: Int) {
-    println("""
-        For $cupsNumber cups of coffee you will need:
-        ${WATER * cupsNumber} ml of water
-        ${MILK * cupsNumber} ml of milk
-        ${COFFEE_BEANS * cupsNumber} g of coffee beans
-    """.trimIndent())
-}
-
 fun main() {
-    println("Write how many cups of coffee you will need:")
-    val cupsNumber: Int = readln().toInt()
-    ingredientCalculator(cupsNumber)
+    println("Write how many ml of water the coffee machine has:")
+    val water: Int = readln().toInt()
+    println("Write how many ml of milk the coffee machine has:")
+    val milk: Int = readln().toInt()
+    println("Write how many grams of coffee beans the coffee machine has:")
+    val coffee_beans: Int = readln().toInt()
 }
