8b3171225759e5ff4f78e3be439cab3b3de319c4
maxim3andr3 <maxim3.vd@gmail.com>
2024-07-22 23:01:42 +0200

    Refactor code to improve readability and maintainability
    - Introduced `Ingredient` class to encapsulate ingredient details (name, unit, quantity).
    - Added `setQuantity` method in `Ingredient` class for updating quantity.
    - Replaced static ingredient variables with a `MutableMap` to store coffee machine ingredients.
    - Implemented a loop to update ingredient quantities based on user input.
    - Enhanced code readability by separating concerns and improving variable names.

--- src/Main.kt
+++ src/Main.kt
@@ -1,10 +1,24 @@
 fun main() {
-    println("Write how many ml of water the coffee machine has:")
-    val water: Int = readln().toInt()
-    println("Write how many ml of milk the coffee machine has:")
-    val milk: Int = readln().toInt()
-    println("Write how many grams of coffee beans the coffee machine has:")
-    val coffeeBeans: Int = readln().toInt()
+    data class Ingredient(val name: String, val unit: String, private var _quantity: Int) {
+        val quantity: Int
+            get() = _quantity
+
+        fun setQuantity(newQuantity: Int) {
+            _quantity = newQuantity
+        }
+    }
+
+    val ingredients: Map<String, Ingredient> = mapOf(
+        "water" to Ingredient(name = "water", unit = "ml", _quantity = 0),
+        "milk" to Ingredient(name = "milk", unit = "ml", _quantity = 0),
+        "coffee beans" to Ingredient(name = "coffee beans", unit = "grams", _quantity = 0)
+    )
+
+    for ((name, ingredient) in ingredients) {
+        println("Write how many ${ingredient.unit} of $name the coffee machine has:")
+        val newQuantity = readln().toInt()
+        ingredient.setQuantity(newQuantity)
+    }
 
     println("Write how many cups of coffee you will need:")
     val cupsOfCoffee: Int = readln().toInt()
