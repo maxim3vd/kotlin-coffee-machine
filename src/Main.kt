afedbc3ead713f5c4e376e0296bd198043c1351a
maxim3andr3 <maxim3.vd@gmail.com>
2024-07-22 22:13:49 +0200

    Implement basic functionality to input and store cups of coffee needed Rename coffee_beans variable to coffeeBeans

--- src/Main.kt
+++ src/Main.kt
@@ -4,5 +4,8 @@ fun main() {
     println("Write how many ml of milk the coffee machine has:")
     val milk: Int = readln().toInt()
     println("Write how many grams of coffee beans the coffee machine has:")
-    val coffee_beans: Int = readln().toInt()
+    val coffeeBeans: Int = readln().toInt()
+
+    println("Write how many cups of coffee you will need:")
+    val cupsOfCoffee: Int = readln().toInt()
 }
